34f6e6dbefc3654917bb5f0bd8b014ec73e89353
Francisco Quintero Coronell <frajaquico@yahoo.es>
2025-09-19 10:04:51 -0500

    Nuevas funciones para alpha s3

--- functions/alpha_functions.sh
+++ functions/alpha_functions.sh
@@ -25,3 +25,33 @@ function alpha_grimoire_pod() {
   latest_pod=$(kubectl get po -n grimoire --sort-by=.metadata.creationTimestamp | tail -1 | awk '{print $1}')
   kubectl exec -it -n grimoire "$latest_pod" -- sh
 }
+
+## Read/Copy to Alpha S3 buckets
+#
+#  Ejemplo:
+#
+#    alpha_s3_ls luna-alpha-workloads-data-lake/
+#
+#    alpha_s3_ls luna-alpha-workloads-data-lake/business-operations/
+#
+#    alpha_s3_ls luna-alpha-workloads-data-lake/business-operations/therapist-forward-fill/
+#
+function alpha_s3_ls() {
+  setaws alpha
+
+  aws s3 ls $1
+}
+
+## Copiar archivo de local a bucket en S3 o viceversa.
+#
+#  Ejemplo:
+#
+#    alpha_s3_cp ~/Downloads/archivo.txt s3://luna-alpha-workloads-data-lake/business-operations/
+#
+#    alpha_s3_cp s3://luna-alpha-workloads-data-lake/business-operations/archivo.txt ~/Downloads/archivo.txt
+#
+function alpha_s3_cp() {
+  setaws alpha
+
+  aws s3 cp $1 $2
+}

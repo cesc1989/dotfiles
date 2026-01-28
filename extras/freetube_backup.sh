b450b44e9b5434f1b312582039cdedf4c95ad24d
Francisco Quintero Coronell <frajaquico@yahoo.es>
2026-01-28 09:41:20 -0500

    fix freetube script

--- extras/freetube_backup.sh
+++ extras/freetube_backup.sh
@@ -8,4 +8,4 @@
 FREETUBE_FOLDER="$HOME/.config/FreeTube"
 DESTINATION_FOLDER="$HOME/Documentos/Documentos/Otro Espacio/Degoogle/FreeTube-bkps"
 
-cp $FREETUBE_FOLDER/{profiles,settings,playlists,history}.db $DESTINATION_FOLDER
+cp $FREETUBE_FOLDER/{profiles,settings,playlists,history}.db "$DESTINATION_FOLDER/"

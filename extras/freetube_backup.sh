cc52698bf9a0b4d29a1e70a553d499ad6965fbc6
Francisco Quintero Coronell <frajaquico@yahoo.es>
2026-01-28 09:44:44 -0500

    freetube script with dated folder destination

--- extras/freetube_backup.sh
+++ extras/freetube_backup.sh
@@ -7,5 +7,12 @@
 
 FREETUBE_FOLDER="$HOME/.config/FreeTube"
 DESTINATION_FOLDER="$HOME/Documentos/Documentos/Otro Espacio/Degoogle/FreeTube-bkps"
+DATED_FOLDER=$(date +%Y-%-m-%d-%H-%M-%S)
 
-cp $FREETUBE_FOLDER/{profiles,settings,playlists,history}.db "$DESTINATION_FOLDER/"
+echo "Creando carpeta destino del backup $DATED_FOLDER"
+echo ""
+mkdir "$DESTINATION_FOLDER/$DATED_FOLDER"
+
+echo "Copiando archivos a carpeta destino $DESTINATION_FOLDER/$DATED_FOLDER"
+echo ""
+cp $FREETUBE_FOLDER/{profiles,settings,playlists,history}.db "$DESTINATION_FOLDER/$DATED_FOLDER/"

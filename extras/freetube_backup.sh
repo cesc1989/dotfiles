f7d3c2ccf9d8550b123b77fdffb374d3cc5a3fa6
Francisco Quintero Coronell <frajaquico@yahoo.es>
2026-01-28 09:38:20 -0500

    script para backupear freetube

---
+++ extras/freetube_backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copia los archivos basicos de uso de FreeTube a la carpeta
+# sincronizada con Mega.
+#
+# FreeTube solo lo uso en Linux Mint asi que la carpeta Documentos está en Español.
+
+FREETUBE_FOLDER="$HOME/.config/FreeTube"
+DESTINATION_FOLDER="$HOME/Documentos/Documentos/Otro Espacio/Degoogle/FreeTube-bkps"
+
+cp $FREETUBE_FOLDER/{profiles,settings,playlists,history}.db $DESTINATION_FOLDER

1be061ef5bfd30576310af94b0c3d33c0f5cb928
Francisco Quintero Coronell <frajaquico@yahoo.es>
2026-05-26 10:38:28 -0500

    activa brave

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -20,9 +20,9 @@ end
 -- Bind the Hyper key
 f18 = hs.hotkey.bind({}, 'F18', enterHyperMode, exitHyperMode)
 
--- hyper:bind({}, "B", function()
---   hs.application.launchOrFocus("Brave Browser")
--- end)
+hyper:bind({}, "B", function()
+  hs.application.launchOrFocus("Brave Browser")
+end)
 
 hyper:bind({}, "F", function()
   hs.application.launchOrFocus("Firefox")
